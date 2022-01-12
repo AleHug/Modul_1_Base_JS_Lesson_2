7ac67b54dcda94f1e5518117fe282a35936f7a3e
AleHug <93007065+AleHug@users.noreply.github.com>
2022-01-12 16:50:45 +0100

    Add files via upload

---
+++ car.js
@@ -0,0 +1,29 @@
+const carName = 'Jeep Cerokee';
+alert (carName);
+
+let maxCarSpeed = '205 km/h';
+alert (maxCarSpeed);
+
+let carOwner = 'Alexander Wa....';
+alert (carOwner);
+
+
+maxCarSpeed = '200 km/h'
+console.log('У машины изменилась скорость',
+maxCarSpeed);
+
+maxCarSpeed = '210 km/h';
+console.log('У машины изменилась скорость', maxCarSpeed);
+
+maxCarSpeed = '190 km/h';
+console.log('У машины изменилась скорость', maxCarSpeed);
+
+
+carOwner = 'Tom';
+console.log('У машины изменился владелец', carOwner);
+
+carOwner = 'Sergey';
+console.log('У машины изменился владелец', carOwner);
+
+carOwner = 'Vladimir';
+console.log('У машины изменился владелец', carOwner);
---
+++ index.js
@@ -0,0 +1,17 @@
+// Comments //
+/* Autor
+Alexander Wagner */
+const myName = 'Alexander';
+console.log('myName');
+
+const mySurname = 'Wa....';
+console.log('mySurname');
+
+let myFavoriteDrink = 'CoC';
+console.log('myFavoriteDrink');
+
+let myFavoriteAnimal = 'Dog';
+console.log('myFavoriteAnimal');
+
+let myFavoriteProgrammingLanguage = 'JavaScript';
+console.log('myFavoriteProgrammingLanguage');
